ba153a64486ae670565d2f3c3114a48abc015f7b
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2023-12-13 23:59:12 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "task_1.h"
 
 int main() {
-    cout << "Task 1" << endl;
+    cout << "Task 1" << endl;/*
     RedBlackTree* RBtree1 = new RedBlackTree();
     // call for task 1
     int N;
@@ -24,11 +24,28 @@ int main() {
 
     cout << "Red-Black Tree:" << endl;
     RBtree1->printTree();
-    cout<<endl;
+    cout<<endl;*/
     cout << "Task 2" << endl;
     // call for task 2
+    RedBlackTree* RBtree2 = new RedBlackTree();
+    // call for task 1
+    int N2;
+    cout << "Enter the number of nodes (N): ";
+    cin >> N2;
+
+    cout << "Enter the values of nodes:" << endl;
+    for (int i = 0; i < N2; i++) {
+        int value;
+        cin >> value;
+        RBtree2->insert2(value);
+    }
+
+    cout << "Red-Black Tree:" << endl;
+    RBtree2->printTree();
+    cout<<endl;
     cout << "Task 3" << endl;
     // call for task 3
+
     cout << "Task 4" << endl;
     // call for task 4
     cout << "Task 5" << endl;
